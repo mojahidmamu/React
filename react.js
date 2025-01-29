222a071a72a4f0e145f63e5954251e5127276764
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-01-29 15:20:05 +0600

    added new file

--- react.js
+++ react.js
@@ -1,3 +1,19 @@
 // React is JS library.It for building user interface.
 // Front-End => what i see, 
 // Back-End => logic, data , 
+
+let number = 0;
+
+const button = document.querySelector('.button');
+const button2 = document.querySelector('.button2');
+const display = document.querySelector('.display')
+
+button.addEventListener(('click'), () => {
+    number++;
+    display.textContent = number;
+})
+
+button2.addEventListener( ('click') , () => {
+    number--;
+    display.textContent = number;
+})
